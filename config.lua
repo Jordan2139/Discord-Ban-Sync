7fc025681412ffae407f564542e7cdeedd9c31d2
Jordan.#2139 <jordansilva213@gmail.com>
2022-12-04 20:34:52 -0500

    Fixed bug and added blacklisted roles

--- config.lua
+++ config.lua
@@ -1,6 +1,11 @@
 Config = {
-    Bot_Token = '', -- Your bot token from https://discord.com/developers/applications (See docs for more)
-    Guild_ID = '', -- The ID for your guild (See docs for more)
-    webhookURL = '', -- Webhook URL to send logs to
-    ThereWasAnIssue = 'Sorry, there was an issue checking if you were banned... Please restart FiveM and if the issue persists contact the server owner.'
+    Bot_Token = 'MTAzMzYwMDIzMDE2MTEyOTU1Mg.G0IdTB.WKc9SXK84q6ww3A1yAB25ZK0b1nlcCJ0F7YSeM', -- Your bot token from https://discord.com/developers/applications (See docs for more)
+    Guild_ID = '907433306616889404', -- The ID for your guild (See docs for more)
+    webhookURL = 'https://discord.com/api/webhooks/1049066583571570729/rfb624nc9ey6ny7oJPGcw1CiLwkPdRMo2L9aYi-jTAFIqwTUPL-20aM3hhDNSAu6nJNh', -- Webhook URL to send logs to
+    ThereWasAnIssue = 'Sorry, there was an issue checking if you were banned... Please restart FiveM and if the issue persists contact the server owner.',
+    steamNotOpen = 'You must have steam open in order to play on this server! Please open steam and relog!',
+    blacklist = {
+        enabled = false, -- Should the script enable blacklisting
+        blacklistedRoles = {'123456789'} -- Array of role ID's that are not allowed to join the server
+    }
 }
